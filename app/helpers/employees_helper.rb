227cca9aeca3cad15ed04a7889db145525e7f51f
Mukarram <mukarram@Mukarrams-MacBook-Pro.local>
2017-09-11 00:24:06 +0530

    CSS added

--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,10 +1,10 @@
 module EmployeesHelper
 
   def nested_employees(employee)
-    content_tag(:ul, :class=>"list-group") do
+    content_tag(:ol) do
       concat(content_tag(:a, "#{employee.name} (#{employee.title})", :href => "employees/#{employee.id}"))
       employee.tree.each do |emp|
-        concat(content_tag(:li,  nested_employees(emp), :class=>"list-group-item",))
+        concat(content_tag(:li,  nested_employees(emp)))
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :employees
   get '/tree', to: "employees#tree"
+  get '/', to: "employees#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
